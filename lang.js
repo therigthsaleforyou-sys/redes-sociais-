987047767959602c4477b328e8d9cd31a4eb0fa8
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-03-02 17:59:24 +0000

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,26 @@
+const LANG = {
+  pt: {
+    title: "Redes Sociais",
+    categories: "Categorias",
+    suggested: "Apps sugeridas",
+    trending: "Apps da moda",
+    weather: "Meteorologia",
+    openBrowser: "Browser",
+    openStore: "Play Store",
+    chooseLang: "Escolher idioma",
+    location: "Localização",
+    back: "Voltar"
+  },
+  en: {
+    title: "Social Networks",
+    categories: "Categories",
+    suggested: "Suggested apps",
+    trending: "Trending apps",
+    weather: "Weather",
+    openBrowser: "Browser",
+    openStore: "Play Store",
+    chooseLang: "Choose language",
+    location: "Location",
+    back: "Back"
+  }
+};
